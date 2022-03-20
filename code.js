3e1205166f27364906882cb7f2feb58cd9cb3bc5
rizanBcodes <100106906+rizanBcodes@users.noreply.github.com>
2022-03-20 08:08:09 +0545

    Create code.js

---
+++ code.js
@@ -0,0 +1,22 @@
+
+//map
+const addedPizza = myPizza.map(
+    function (value){
+        return '<p>' + value + '</p>';
+    }
+)
+console.log(addedPizza);
+
+//filter
+const Filtered = myPizza.filter(value => value >= 3)
+
+console.log(Filtered);
+
+//reduce
+const myPizza = [2, 3, 4, 5, 6, -2, 5];
+
+let valueee = myPizza.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;    
+}, 0)
+
+console.log(valueee);
